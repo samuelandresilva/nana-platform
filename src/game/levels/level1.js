9b7e559e32450096a286dd7233dbbe9dcf6ea9b1
Samuel André <samuel.asilva@icloud.com>
2026-01-28 15:56:45 -0300

    refactor: extract level data

---
+++ src/game/levels/level1.js
@@ -0,0 +1,19 @@
+export const LEVEL_1 = {
+    groundHoles: [
+        { x: 1080, width: 150 },
+        { x: 1580, width: 150 },
+        { x: 2180, width: 150 }
+    ],
+    obstacles: [
+        { x: 600, y: 550, width: 350, height: 40 },
+        { x: 950, y: 420, width: 100, height: 40 },
+        { x: 1350, y: 300, width: 180, height: 40 },
+        { x: 1400, y: 540, width: 140, height: 40 },
+        { x: 1600, y: 420, width: 140, height: 40 },
+        { x: 1800, y: 550, width: 140, height: 40 },
+        { x: 2350, y: 420, width: 100, height: 40 },
+        { x: 2500, y: 550, width: 140, height: 40 },
+        { x: 2800, y: 550, width: 80, height: 40 },
+        { x: 3000, y: 600, width: 140, height: 40 }
+    ]
+};
